bb241252f8785e42297fa70466485797a35a93ab
Krushna Dasarwad <krushnadasarwad2@gmail.com>
2025-11-20 18:43:26 +0530

    change

---
+++ Day30/harry/main.js
@@ -0,0 +1,9 @@
+//Methods
+// -------------------CommonJs------------
+// let a = require("./server.js");
+// console.log(a);
+
+//-------------------EcmaScript--------------
+import {p,q,r,s} from "./server.js"
+console.log(p,q,r,s);
+
---
+++ Day30/harry/server.js
@@ -0,0 +1,13 @@
+//Methods
+// -------------------CommonJs------------
+// const a =1;
+// const b =2;
+// const c =3;
+// const d =4;
+// module.exports = {a,b,c,d};
+
+//-------------------EcmaScript--------------
+export const p =1;
+export const q =2;
+export const r =3 ;
+export const s =4;
